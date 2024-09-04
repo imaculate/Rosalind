964cd34fd205b20b603f0e2143340868f51a19e0
Imaculate <imaculatemosha@yahoo.com>
2024-09-04 08:13:32 -0700

    Superstring

--- SuperString.java
+++ SuperString.java
@@ -124,7 +124,7 @@ public class SuperString {
     public static int[] buildKmpShift(String s)
     {
         int N = s.length();
-        int[] z = buildkmpPrefix(s), shifts = new int[N];
+        int[] z = buildkmpZ(s), shifts = new int[N];
         // consider using z matrix
         for (int i = 0; i< N; i++)
         {
@@ -155,8 +155,50 @@ public class SuperString {
             if (s.charAt(i) == s.charAt(j)) j++;
             result[i] = j;
         }
-        // System.out.println("KMPrefix array is: " + Arrays.toString(result));
+        System.out.println("KMPrefix array is: " + Arrays.toString(result));
         return result;
+    }
+
+    public static int[] buildkmpZ(String s)
+    {
+        int N = s.length();
+        int[] z = new int[N];
+        int left = 0, right = 0, length = 0;
+        for (int i = 1; i < N; i++)
+        {
+            length = 0;
+            if (i < right)
+            {
+                /*
+                # Being a z-box, we have:
+                # pattern[l, r) = pattern[0, r - l)
+                #
+                # Since l < i < r, we have:
+                # pattern[i, r) = pattern[i - l, r - l)
+                #
+                # We have already computed z[i - l], so we have
+                # pattern[i - l, i - l + z[i - l]) = pattern[0, z[i - l])
+                #
+                # Therefore we can conclude the below, as a starting point before the search begins
+                */
+                length = Math.min(z[i - left], right - i);
+            }
+
+            // Extend z-box to the right
+            while ((i + length < N) && (s.charAt(i + length) == s.charAt(length)))
+            {
+                length++;
+            }
+
+            z[i] = length;
+            if (i + length > right)
+            {
+                left = i;
+                right = i + length;
+            }
+        }
+        System.out.println("KMpz array is: " + Arrays.toString(z));
+        return z;
     }
 
 
